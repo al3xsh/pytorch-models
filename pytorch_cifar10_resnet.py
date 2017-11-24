84c1d69d555c125625d97e695d1ac6278f9c8278
Alex Shenfield <alex.shenfield@gmail.com>
2017-11-24 13:17:41 +0000

    first commit - working code to train resnet (110) with preactivations on cifar10. achieves c95% accuracy in approximately 4 hours
    Former-commit-id: f2dc0396b6c705767dbd7efcf2895f7149962be4

---
+++ pytorch_cifar10_resnet.py
@@ -0,0 +1,223 @@
+"""
+pytorch_resnet_110.py
+
+
+"""
+
+import torch.nn as nn
+import math
+
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+    
+# original bottle neck block
+class OriginalBottleneckBlock(nn.Module):
+    expansion = 1
+
+    # this is where all the layers are defined
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        
+        # what this does is make sure we call all the methods in the right 
+        # order
+        # see https://stackoverflow.com/a/27134600
+        
+        # python 3 allows use of super like this ...
+        super().__init__()
+        #super(BasicBlock, self).__init__() # python 2 version
+        
+        # define our layers
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    # this where all the layers are put together
+    def forward(self, x):
+        
+        # x is our input 
+        residual = x
+
+        # build the convolutional path (using old resnet style ...)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        # if we are down sampling in this block, we include the "downsample"
+        # block in the shortcut path - this is the 1x1 convolution with stride
+        # of two (followed by batch norm)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        # so we add the residual (shortcut path) and apply relu to the result
+        out += residual
+        out = self.relu(out)
+
+        return out
+    
+
+# basic original residual block
+class OriginalBasicBlock(nn.Module):
+    expansion = 1
+
+    # this is where all the layers are defined
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        
+        # what this does is make sure we call all the methods in the right 
+        # order
+        # see https://stackoverflow.com/a/27134600
+        
+        # python 3 allows use of super like this ...
+        super().__init__()
+        #super(BasicBlock, self).__init__() # python 2 version
+        
+        # define our layers
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    # this where all the layers are put together
+    def forward(self, x):
+        
+        # x is our input 
+        residual = x
+
+        # build the convolutional path (using old resnet style ...)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        # if we are down sampling in this block, we include the "downsample"
+        # block in the shortcut path - this is the 1x1 convolution with stride
+        # of two (followed by batch norm)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        # so we add the residual (shortcut path) and apply relu to the result
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+
+class ResNetCifar10(nn.Module):
+
+    def __init__(self, block, depth, num_classes=10):
+                
+        super().__init__()
+        #super(ResNetCifar10, self).__init__()
+        
+        #Model type specifies number of layers for CIFAR-10 and CIFAR-100 model
+        assert (depth - 2) % 6 == 0, 'depth should be one of 20, 32, 44, 56, 110'
+        layer_blocks = (depth - 2) // 6
+        print ('CifarResNet : Depth : {} , Layers for each block : {}'.format(depth, layer_blocks))
+        
+        # first convolutional layer + batchnorm + relu
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        
+        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        # layer up the residual blocks
+        self.inplanes = 16
+        self.layer1 = self.layer(block, 16, layer_blocks, stride=1)
+        self.layer2 = self.layer(block, 32, layer_blocks, stride=2)
+        self.layer3 = self.layer(block, 64, layer_blocks, stride=2)
+        
+        # add global average pooling 
+        self.avgpool = nn.AvgPool2d(8, stride=1)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal(m.weight)
+                m.bias.data.zero_()
+
+    # lets layer up the residual blocks ...
+    def layer(self, block, planes, blocks, stride=1):
+        
+        # down sample on the shortcut path if needed 
+        #
+        # (i.e. the first residual block in each set - apart from the very 
+        # first set, where the input dimension - 16 - is the same as the 
+        # output dimension of the convolutional path - 16 - because of the 
+        # very first convolutional layer)
+        downsample = None
+        if (stride != 1) or (self.inplanes != (planes * block.expansion)):
+            
+            # down sample by adding a convolution layer with 1x1 filters and
+            # 2x2 stride
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        # build all the residual blocks in this layer
+        layers = []
+        
+        # the first residual block in this layer will use downsampling 
+        # _unless_ its the very first residual layer
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        # return the layered residual blocks
+        return nn.Sequential(*layers)
+
+    # this where all the layers are put together
+    def forward(self, x):
+        
+        # first convolution layer + batch norm +relu
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        # residual layers
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        # global average pooling
+        x = self.avgpool(x)
+        
+        # confusingly - view reshapes the tensor ...
+        x = x.view(x.size(0), -1)
+        
+        # our fully connected final layer
+        x = self.fc(x)
+
+        return x
+    
+    
+def resnet110(**kwargs):
+    """Constructs a ResNet-110 model.
+    """
+    model = ResNetCifar10(BasicBlock, 110, **kwargs)
+    return model
---
+++ train_cifar10.py
@@ -0,0 +1,355 @@
+"""
+train_cifar10.py
+
+much of this training code is heavily influenced by the imagenet training 
+code in pytorch/examples
+
+author:     Alex Shenfield
+date:       20/11/2017
+
+"""
+
+# imports
+
+# import some basic os level stuff
+import shutil
+import time
+
+# import pytorch stuff
+import torch
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+# import my version of resnet110
+from cifar_resnet_v2 import resnet110
+
+#
+# set up some functions to train for an epoch and then run on validation data
+#
+
+# train for a single epoch
+def train(epoch, train_loader, model, criterion, optimiser):
+        
+    # set up some measurements
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    
+    # switch the network to training mode
+    model.train()
+
+    # get the start time
+    start = time.time()
+
+    # get the data one batch at a time
+    for batch_ix, (input, target) in enumerate(train_loader):
+        
+        # update the data loading time
+        data_time.update(time.time() - start)
+        
+        # get the targets and inputs for the model (and cudarise them ...)
+        input = input.cuda(async=True)
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+        
+        # compute output and loss (forward pass)
+        output = model(input_var)
+        loss   = criterion(output, target_var)
+        
+        # calculate accuracy        
+        precision = accuracy(output.data, target, topk=(1,5))
+        
+        # record accuracy and loss
+        losses.update(loss.data[0],  input.size(0))
+        top1.update(precision[0][0], input.size(0))
+        top5.update(precision[1][0], input.size(0))
+        
+        # compute gradients from the backwards pass and do the optimisation 
+        # step
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+        
+        # update the elapsed time
+        batch_time.update(time.time() - start)
+        start = time.time()
+        
+        # show some progress (after every batch)
+        print('Epoch: [{0}][{1}/{2}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+              'Precision@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+              'Precision@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      epoch, batch_ix, len(train_loader), 
+                      batch_time=batch_time, 
+                      data_time=data_time, 
+                      loss=losses, 
+                      top1=top1, 
+                      top5=top5))
+        
+
+# validate on the unseen validation data (I am trying to get away from the 
+# train and test descriptions as testing should be completely unseen data 
+# that isn't used anywhere for model selection
+#
+# see http://www.fast.ai/2017/11/13/validation-sets/ for a compelling argument
+# as to why current practice is - often - rubbish)
+def validate(val_loader, model, criterion):
+        
+    # set up some measurements
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+    
+    # switch the network to training mode
+    model.eval()
+
+    # get the start time
+    start = time.time()
+
+    # get the data one batch at a time
+    for batch_ix, (input, target) in enumerate(val_loader):
+        
+        # get the targets and inputs for the model (and cudarise them ...)
+        input = input.cuda(async=True)
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+        
+        # compute output and loss
+        output = model(input_var)
+        loss   = criterion(output, target_var)
+        
+        # calculate accuracy        
+        precision = accuracy(output.data, target, topk=(1,5))
+        
+        # record accuracy and loss
+        losses.update(loss.data[0],  input.size(0))
+        top1.update(precision[0][0], input.size(0))
+        top5.update(precision[1][0], input.size(0))
+        
+        # update the elapsed time
+        batch_time.update(time.time() - start)
+        start = time.time()
+        
+        # show some progress (after every batch)
+        print('Test: [{0}/{1}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+              'Precision@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+              'Precision@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      batch_ix, len(val_loader), 
+                      batch_time=batch_time,
+                      loss=losses, 
+                      top1=top1, 
+                      top5=top5))
+    
+    # display overall testing results
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg
+    
+
+#
+# miscellaneous functions for saving data, recording data, and adjusting 
+# the learning rate
+#
+
+# checkpoint the training progress and save the best model parameters found
+# so far
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')    
+
+
+# adjust the learning rate accoridng to the supplied schedule (i.e. use the 
+# gamma variable to decay the learning rate)
+def adjust_learning_rate(optimiser, epoch, schedule, gamma):
+    
+    # every time the current epoch is found in the schedule, decay the 
+    # learning rate by gamma
+    if epoch in schedule:
+        for param_group in optimiser.param_groups:
+            param_group['lr'] = param_group['lr'] * gamma
+            
+            # print an update so we can check all is well
+            print('epoch = {0}, lr = {1}'.format(epoch, param_group['lr']))
+
+
+# compute accuracy (or - more correctly - precision @ k)
+def accuracy(output, target, topk=(1,)):
+    
+    # get the largest value of the topk tuple, and the size of the batch we 
+    # are calculating accuracy for 
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    # get the predicted class and the ground truth class
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    # calculate how well we are doing with respect to every k supplied in the 
+    # "topk" input 
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+# define our AverageMeter class for recording training / testing data
+class AverageMeter(object):
+    
+    # every time we create a new AverageMeter object then zero the data
+    def __init__(self):
+        self.reset()
+
+    # zero the data
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    # update the data - keeping a running total and average
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+#
+# main code
+#
+    
+# this is the main part of the code where we run everything!
+def main():
+
+    # set up initial parameters
+    
+    # training parameters
+    max_epochs = 200
+    batch_size = 64
+    
+    best_accuracy = 0
+    
+    # optimisation parameters for optimiser (sgd)
+    lr = 0.1
+    momentum = 0.9
+    weight_decay = 0.0001   # nb - this is l2 reqularisation ...
+    schedule = [80, 120]
+    gamma = 0.1
+    
+    # load the data set
+    
+    # print progress
+    print("loading dataset - cifar10")
+    
+    # get the per pixel means and standard deviation for each channel
+    # (precalculated - i assume on the training data ...)
+    mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+    std = [x / 255 for x in [63.0, 62.1, 66.7]]
+    
+    # define the train and test transforms
+    
+    # for training we we flip, crop, and subtract the per pixel mean
+    # (i think these are whats listed in the paper)
+    train_transform = transforms.Compose(
+            [transforms.RandomHorizontalFlip(), 
+             transforms.RandomCrop(32, padding=4), 
+             transforms.ToTensor(),
+             transforms.Normalize(mean, std)])
+    
+    # for the test data we just subtract the mean
+    test_transform = transforms.Compose(
+            [transforms.ToTensor(), 
+             transforms.Normalize(mean, std)])
+    
+    # get the dataset (split into test and train)
+    train_data = dset.CIFAR10(root='./data', train=True, 
+                              transform=train_transform, download=True)
+    test_data  = dset.CIFAR10(root='./data', train=False, 
+                             transform=test_transform, download=True)
+    
+     # define some loaders for our data
+    train_loader = torch.utils.data.DataLoader(train_data, 
+                                               batch_size=batch_size, 
+                                               shuffle=True,
+                                               num_workers=2, 
+                                               pin_memory=True)
+    val_loader   = torch.utils.data.DataLoader(test_data, 
+                                               batch_size=batch_size, 
+                                               shuffle=False,
+                                               num_workers=2, 
+                                               pin_memory=True)
+    
+    # create model
+    
+    # print progress
+    print("creating resnet 110 model")
+       
+    # create the network and cuda it ...
+    net = resnet110()
+    net.cuda()
+    
+    # assign it to run on all available gpus
+    net = torch.nn.DataParallel(net)    
+    
+    # enable benchmarking in cudnn to allow autotuning of the algorithms 
+    # (this can speed up the runtime - as long as the model inputs remain the
+    # same size)
+    torch.backends.cudnn.benchmark = True
+    
+    # define loss function (criterion) and optimiser
+    loss_function = torch.nn.CrossEntropyLoss().cuda()
+    sgd_optimiser = torch.optim.SGD(net.parameters(), lr, 
+                                    momentum=momentum, 
+                                    weight_decay=weight_decay, 
+                                    nesterov=True)
+    
+    # train the network
+    print("training resnet 110 model")
+    
+    # train the network
+    for epoch in range(max_epochs):
+        
+        # adjust the learning rate of the optimiser according to the 
+        # learning rate schedule
+        adjust_learning_rate(sgd_optimiser, epoch, schedule, gamma)
+        
+        # train for a single epoch
+        train(epoch, train_loader, 
+              model=net, criterion=loss_function, optimiser=sgd_optimiser)
+        
+        # evaluate on th validation set
+        accuracy = validate(val_loader, model=net, criterion=loss_function)
+        
+        # remember the best accuracy (precision@1) and save the model 
+        # checkpoint
+        is_best = accuracy > best_accuracy
+        best_accuracy = max(accuracy, best_accuracy)
+        save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': 'resnet110',
+                'best_accuracy': best_accuracy,
+                'optimiser_state': sgd_optimiser.state_dict(),
+                'model_state': net.state_dict()
+                }, is_best)
+        
+    # and that's all she wrote :)    
+    print('Finished Training')
+    
+#
+# program entry point
+#
+if __name__ == '__main__':
+    main()
